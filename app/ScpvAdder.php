614dddc992f7333d17c8546a8925ea9a6c4cd797
Alpesh Maru <alpeshmaru@360incemail.com>
2026-06-10 16:28:13 +0530

    SCPV Model and Controllers Added New Files
    SCPV Model and Controllers Added New Files

--- app/ScpvAdder.php
+++ app/ScpvAdder.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ScvpAssemblyCostPcPk extends Model
+class ScpvAdder extends Model
 {
     //
 }
--- app/ScpvMaterial.php
+++ app/ScpvMaterial.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class ScvpMasterMotorPrice extends Model
+class ScpvMaterial extends Model
 {
     //
 }
